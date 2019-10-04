31b39f94bb0561bc79b5c280272640593170f377
offish <overutilization@gmail.com>
2019-10-05 01:13:16 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -12,6 +12,7 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/offish/steam_community_market",
+    download_url='https://github.com/offish/steam_community_market/archive/v1.0.0.tar.gz',
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",

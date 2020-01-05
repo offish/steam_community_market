07de211a79cec4545da6c4ca03f85df3d43f639b
offish <overutilization@gmail.com>
2020-01-05 14:51:34 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,14 +5,14 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="steam_community_market",
-    version="1.0.0",
+    version="1.1.0",
     author="offish",
     author_email="overutilization@gmail.com",
     description="Easily get item prices and volumes from the Steam Community Market using Python 3",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/offish/steam_community_market",
-    download_url='https://github.com/offish/steam_community_market/archive/v1.0.0.tar.gz',
+    download_url='https://github.com/offish/steam_community_market/archive/v1.1.0.tar.gz',
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",

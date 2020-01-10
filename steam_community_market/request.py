67705445b563b019948ac91f5211bdbd023a602a
offish <overutilization@gmail.com>
2020-01-10 22:45:18 +0100

    Update request.py

--- steam_community_market/request.py
+++ steam_community_market/request.py
@@ -5,9 +5,7 @@ import json
 def request(url: str, payload: dict) -> dict:
     r = requests.get(url, payload)
 
-    if r.ok:
-        return r.json()
     try:
         return json.loads(r.text)
     except ValueError:
-        return {'success': False, 'code': r.status_code, 'reason': r.text}
+        return {'success': False, 'status_code': r.status_code, 'text': r.text}

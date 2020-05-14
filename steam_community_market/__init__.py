2063bc937aa61a05bdc0deadeb2f5359779f10cb
offish <overutilization@gmail.com>
2020-05-14 22:45:43 +0200

    Add files via upload

--- steam_community_market/__init__.py
+++ steam_community_market/__init__.py
@@ -5,10 +5,10 @@ Get item prices and volumes from the Steam Community Market using Python 3
 """
 
 
-__title__ = 'steam_community_market'
-__author__ = 'offish'
-__license__ = 'MIT'
-__version__ = '1.2.1'
+__title__ = "steam_community_market"
+__author__ = "offish"
+__license__ = "MIT"
+__version__ = "1.2.2"
 
 from .market import Market
 from .request import request

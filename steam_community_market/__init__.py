e598bc4c8f9b366aa431e9833951988b319632f2
RoachxD <nicolae.adrian.m@gmail.com>
2023-04-07 14:40:11 +0300

    Add `all` variable to export public members from this module
     - Defined `all` variable to list public members for when this module will be imported.
     - Ensure only `AppID`, `ESteamCurrency`, `Market`, and `request` are available for importing.
    
    This commit adds the `all` variable to the script, explicitly defining the public members of the module that should be available for import when using a wildcard import statement (e.g. `from steam_community_market import *`). This helps maintain a clean API and prevents unintended imports of private or internal members.

--- steam_community_market/__init__.py
+++ steam_community_market/__init__.py
@@ -3,13 +3,17 @@ steam_community_market
 ======================
 Get item prices and volumes from the Steam Community Market using Python 3
 """
-
+from .enums import AppID, ESteamCurrency
+from .market import Market
+from .request import request
 
 __title__ = "steam_community_market"
 __author__ = "offish"
 __license__ = "MIT"
 __version__ = "1.2.3"
-
-from .market import Market
-from .request import request
-from .enums import ESteamCurrency, AppID
+__all__ = [
+    "AppID",
+    "ESteamCurrency",
+    "Market",
+    "request",
+]

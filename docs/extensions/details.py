4a78de3902e51367fa700d0bab674eec3783e51c
RoachxD <nicolae.adrian.m@gmail.com>
2023-04-07 14:21:41 +0300

    Optimize and refactor `extensions` scripts
    `extensions/builder.py`:
    - Use list comprehension for indexcounts generation.
    - Replace string concatenation with `f-string` in `handle_page` call for `genindex-single.html`.
    
    `extensions/details.py`:
    - Change class names to follow Python's naming convention (capitalized class names).
    - Remove extra line breaks within function bodies.
    
    `extensions/exception_hierarchy.py`:
    - Rename `exception_hierarchy` class to follow Python's naming convention (capitalized class names)
    
    This commit includes improvements to the extensions scripts, making them more efficient, readable, and consistent.

--- docs/extensions/details.py
+++ docs/extensions/details.py
@@ -4,31 +4,33 @@ from docutils.parsers.rst import directives
 from docutils.parsers.rst.roles import set_classes
 
 
-class details(nodes.General, nodes.Element):
+class Details(nodes.General, nodes.Element):
     pass
 
 
-class summary(nodes.General, nodes.Element):
+class Summary(nodes.General, nodes.Element):
     pass
 
 
 def visit_details_node(self, node):
-    self.body.append(self.starttag(
-        node, 'details', CLASS=node.attributes.get('class', '')))
+    self.body.append(
+        self.starttag(node, "details", CLASS=node.attributes.get("class", ""))
+    )
 
 
 def visit_summary_node(self, node):
-    self.body.append(self.starttag(
-        node, 'summary', CLASS=node.attributes.get('summary-class', '')))
+    self.body.append(
+        self.starttag(node, "summary", CLASS=node.attributes.get("summary-class", ""))
+    )
     self.body.append(node.rawsource)
 
 
 def depart_details_node(self, node):
-    self.body.append('</details>\n')
+    self.body.append("</details>\n")
 
 
 def depart_summary_node(self, node):
-    self.body.append('</summary>')
+    self.body.append("</summary>")
 
 
 class DetailsDirective(Directive):
@@ -36,8 +38,8 @@ class DetailsDirective(Directive):
     optional_arguments = 1
 
     option_spec = {
-        'class': directives.class_option,
-        'summary-class': directives.class_option,
+        "class": directives.class_option,
+        "summary-class": directives.class_option,
     }
 
     has_content = True
@@ -46,13 +48,15 @@ class DetailsDirective(Directive):
         set_classes(self.options)
         self.assert_has_content()
 
-        text = '\n'.join(self.content)
-        node = details(text, **self.options)
+        text = "\n".join(self.content)
+        node = Details(text, **self.options)
 
         if self.arguments:
-            summary_node = summary(self.arguments[0], **self.options)
-            summary_node.source, summary_node.line = self.state_machine.get_source_and_line(
-                self.lineno)
+            summary_node = Summary(self.arguments[0], **self.options)
+            (
+                summary_node.source,
+                summary_node.line,
+            ) = self.state_machine.get_source_and_line(self.lineno)
             node += summary_node
 
         self.state.nested_parse(self.content, self.content_offset, node)
@@ -60,6 +64,6 @@ class DetailsDirective(Directive):
 
 
 def setup(app):
-    app.add_node(details, html=(visit_details_node, depart_details_node))
-    app.add_node(summary, html=(visit_summary_node, depart_summary_node))
-    app.add_directive('details', DetailsDirective)
+    app.add_node(Details, html=(visit_details_node, depart_details_node))
+    app.add_node(Summary, html=(visit_summary_node, depart_summary_node))
+    app.add_directive("details", DetailsDirective)
--- docs/extensions/exception_hierarchy.py
+++ docs/extensions/exception_hierarchy.py
@@ -2,17 +2,16 @@ from docutils import nodes
 from docutils.parsers.rst import Directive
 
 
-class exception_hierarchy(nodes.General, nodes.Element):
+class ExceptionHierarchy(nodes.General, nodes.Element):
     pass
 
 
 def visit_exception_hierarchy_node(self, node):
-    self.body.append(self.starttag(
-        node, 'div', CLASS='exception-hierarchy-content'))
+    self.body.append(self.starttag(node, "div", CLASS="exception-hierarchy-content"))
 
 
 def depart_exception_hierarchy_node(self, node):
-    self.body.append('</div>\n')
+    self.body.append("</div>\n")
 
 
 class ExceptionHierarchyDirective(Directive):
@@ -20,12 +19,14 @@ class ExceptionHierarchyDirective(Directive):
 
     def run(self):
         self.assert_has_content()
-        node = exception_hierarchy('\n'.join(self.content))
+        node = ExceptionHierarchy("\n".join(self.content))
         self.state.nested_parse(self.content, self.content_offset, node)
         return [node]
 
 
 def setup(app):
-    app.add_node(exception_hierarchy, html=(
-        visit_exception_hierarchy_node, depart_exception_hierarchy_node))
-    app.add_directive('exception_hierarchy', ExceptionHierarchyDirective)
+    app.add_node(
+        ExceptionHierarchy,
+        html=(visit_exception_hierarchy_node, depart_exception_hierarchy_node),
+    )
+    app.add_directive("exception_hierarchy", ExceptionHierarchyDirective)

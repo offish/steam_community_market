7873a737f5fbd1d545d8b3db0b2367f2cb90c260
RoachxD <nicolae.adrian.m@gmail.com>
2023-04-08 05:30:51 +0300

    Added more app ids to the `AppID` enum  - Documented their real name.

--- steam_community_market/enums.py
+++ steam_community_market/enums.py
@@ -174,8 +174,35 @@ class ESteamCurrency(enum.IntEnum):
 class AppID(enum.IntEnum):
     """A short list of Steam apps that have a community market."""
 
+    #: Team Fortress 2
     TF2 = 440
+
+    #: Dota 2
     DOTA2 = 570
+
+    #: Counter-Strike: Global Offensive
     CSGO = 730
+
+    #: Steam
     STEAM = 753
+
+    #: Don't Starve
+    DS = 219740
+
+    #: Killing Floor 2
+    KF2 = 232090
+
+    #: SteamVR
+    STEAMVR = 250820
+
+    #: Unturned
+    UNTURNED = 304930
+
+    #: Don't Starve Together
+    DST = 322330
+
+    #: PUBG: PlayerUnknown's Battlegrounds
+    PUBG = 578080
+
+    #: Rust
     RUST = 252490

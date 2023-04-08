696c7baf631b62ddda77522e4f50c001ef9c2836
RoachxD <nicolae.adrian.m@gmail.com>
2023-04-08 04:26:46 +0300

    Added `REQUEST_HEADERS`  - Generic headers have been added for when interacting with the Steam Community Market API.

--- steam_community_market/request.py
+++ steam_community_market/request.py
@@ -1,5 +1,20 @@
 import requests
 
+#: Generic headers to send with each request to the Steam Community Market API.
+REQUEST_HEADERS = {
+    "Accept": "*/*",
+    "Accept-Encoding": "gzip, deflate, br",
+    "Accept-Language": "en-US,en;q=0.9",
+    "Connection": "keep-alive",
+    "DNT": "1",
+    "Host": "steamcommunity.com",
+    "Referer": "https://steamcommunity.com/market/",
+    "Sec-Fetch-Dest": "empty",
+    "Sec-Fetch-Mode": "cors",
+    "Sec-Fetch-Site": "same-origin",
+    "X-Requested-With": "XMLHttpRequest",
+}
+
 
 def request(url: str, payload: dict) -> dict:
     """Make a request to the Steam Community Market API.
@@ -12,7 +27,7 @@ def request(url: str, payload: dict) -> dict:
     :rtype: dict
     """
     try:
-        response = requests.get(url, params=payload)
+        response = requests.get(url, params=payload, headers=REQUEST_HEADERS)
         response.raise_for_status()
         return response.json()
     except (requests.exceptions.RequestException, ValueError):
